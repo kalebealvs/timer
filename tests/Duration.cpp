42dd400b8ab031ff2c14fa7b30186fef665da93a
Kalebe Alves <kalebeapsoares@gmail.com>
2022-05-25 03:34:47 +0200

    Atomic members for timer

--- tests/Duration.cpp
+++ tests/Duration.cpp
@@ -8,6 +8,14 @@
 using namespace ::testing;
 using namespace std::this_thread;
 using namespace std::chrono;
+
+using Clock = high_resolution_clock;
+using Time = Clock::time_point;
+using sc = seconds;
+using ms = milliseconds;
+using mc = microseconds;
+using ns = nanoseconds;
+
 TEST(TimerDuration, Elapsed) {
     Timer t;
     sleep_for(milliseconds(2001));
